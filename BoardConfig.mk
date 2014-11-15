64045f1b1d383dc77a913ae779fb9380179fab9b
Ivan Grinko <iivanich@gmail.com>
2014-11-15 22:35:19 +0200

    pyramid: Remove SELinux policy
    * Rely on QCOM common SELinux policy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -95,32 +95,6 @@ TARGET_POWERHAL_VARIANT := cm
 # Lights
 TARGET_PROVIDES_LIBLIGHT := true
 
-# SELinux
-BOARD_SEPOLICY_DIRS += \
-    device/htc/pyramid/sepolicy
-
-BOARD_SEPOLICY_UNION += \
-    app.te \
-    bluetooth.te \
-    device.te \
-    domain.te \
-    drmserver.te \
-    file_contexts \
-    file.te \
-    hci_init.te \
-    healthd.te \
-    init_shell.te \
-    init.te \
-    keystore.te \
-    kickstart.te \
-    mediaserver.te \
-    rild.te \
-    surfaceflinger.te \
-    system.te \
-    ueventd.te \
-    wpa_socket.te \
-    wpa.te
-
 # Webkit
 ENABLE_WEBGL := true
 TARGET_FORCE_CPU_UPLOAD := true
