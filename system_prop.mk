d49cb3599d7562dfc49f1dd67ac9d9c6087c10f5
Ivan Grinko <iivanich@gmail.com>
2014-12-11 20:37:32 +0200

    pyramid: Use system_prop.mk instead of system.prop

--- system_prop.mk
+++ system_prop.mk
@@ -1,3 +1,32 @@
+#
+# system_prop.mk for pyramid
+#
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.sf.lcd_density=240
+
+# radio
+PRODUCT_PROPERTY_OVERRIDES += \
+    mobiledata.interfaces=rmnet0 \
+    rild.libpath=/system/lib/libhtc_ril.so \
+    ro.telephony.ril_class=HtcMsm8660RIL
+
+# wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+ wifi.interface=wlan0
+
+# UMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.sys.usb.config=mtp,adb \
+    ro.vold.umsdirtyratio=20
+
+# tweaks
+PRODUCT_PROPERTY_OVERRIDES += \
+     dalvik.vm.dexopt-data-only=1 \
+     dalvik.vm.dexopt-flags=v=a,o=v,m=y,u=n \
+     windowsmgr.max_events_per_sec=300 \
+     pm.sleep_mode=1
+
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
     com.qc.hardware=true \
@@ -8,6 +37,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     dev.pm.dyn_samplingrate=1 \
     ro.opengles.version=131072 \
     debug.egl.recordable.rgba8888=1
+
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
     media.stagefright.use-awesome=true
