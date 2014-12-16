45d4501250eb91d7ea7742ecac6422a2c39f3e56
Ivan Grinko <iivanich@gmail.com>
2014-12-16 11:54:01 +0200

    pyramid: Inherit common QCOM SELinux policy

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -143,6 +143,9 @@ BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 # Misc
 BOARD_USES_LEGACY_MMAP := true
 
+# SELinux
+-include device/qcom/sepolicy/sepolicy.mk
+
 # Filesystem
 TARGET_USERIMAGES_USE_EXT4 := true
 BOARD_VOLD_MAX_PARTITIONS := 36
