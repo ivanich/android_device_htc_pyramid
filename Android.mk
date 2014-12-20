e747620c3d101a1af48998aa319491e56762559f
Ivan Grinko <iivanich@gmail.com>
2014-12-20 03:10:02 +0200

    pyramid: Create firmware symlinks at build time

--- Android.mk
+++ Android.mk
@@ -18,5 +18,32 @@ ifeq ($(TARGET_BOARD_PLATFORM),msm8660)
 LOCAL_PATH := $(call my-dir)
 
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+FIRMWARE_MODEM_IMAGES := \
+    modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 modem.b06 modem.b07 modem.b08 modem.b09 modem.mdt
+
+FIRMWARE_MODEM_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_MODEM_IMAGES)))
+$(FIRMWARE_MODEM_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Modem Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware_radio/$(notdir $(subst modem_fw,modem_f2,$@)) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_MODEM_SYMLINKS)
+
+FIRMWARE_Q6_IMAGES := \
+    q6.b00 q6.b01 q6.b02 q6.b03 q6.b04 q6.b05 q6.b06 q6.b07 q6.b08 q6.b09 q6.mdt
+
+FIRMWARE_Q6_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_Q6_IMAGES)))
+$(FIRMWARE_Q6_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Q6 Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware_q6/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_Q6_SYMLINKS)
+
+
+
 endif
 endif
