61676ff02a0c77589779331afa459e5ae2e9a1ca
Ivan Grinko <iivanich@gmail.com>
2015-04-17 22:03:41 +0300

    Revert "pyramid: Add qcom perf lib (from GS2)"
    This reverts commit 9d7cb78aa92d05e6c0553226a9a08516e1e53cc3.

--- system_prop.mk
+++ system_prop.mk
@@ -36,7 +36,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.hw=1 \
     dev.pm.dyn_samplingrate=1 \
     ro.opengles.version=131072 \
-    ro.vendor.extension_library=/system/lib/libqc-opt.so \
     debug.egl.recordable.rgba8888=1
 
 ## misc
