0e67b8db5e3d04b86ed026ae9c6efc9020bdbaf0
Shantur Rathore <shantur.rathore@gmail.com>
2015-04-26 15:17:19 +0100

    Fix portrait mode video recording.
    As per : https://github.com/OpenDesireProject/android_device_htc_msm7x30-common/commit/33e9ea09893b635cd8a99d7b1291e7e9f26e7db2

--- camera/CameraWrapper.cpp
+++ camera/CameraWrapper.cpp
@@ -167,6 +167,12 @@ static char *camera_fixup_setparams(int id, const char *settings)
     else
 	params.set("caf-focus-mode", "default");
 
+    bool isVideo = !strcmp(params.get(android::CameraParameters::KEY_RECORDING_HINT), "true");
+
+    if (isVideo) {
+        params.set(android::CameraParameters::KEY_ROTATION, "0");
+    }
+
 #if !LOG_NDEBUG
     ALOGV("%s: fixed parameters:", __FUNCTION__);
     params.dump();
