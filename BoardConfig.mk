69502c22335cf27a1905d339d6e409f7e1038c45
Ivan Grinko <iivanich@gmail.com>
2015-05-01 12:32:14 +0300

    recovery: Use new ION api for twrp

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -179,5 +179,6 @@ TW_NO_SCREEN_BLANK := true
 TW_INCLUDE_DUMLOCK := true
 HAVE_SELINUX := true
 TW_NO_CPU_TEMP := true
+TW_NEW_ION_HEAP := true
 TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
 TARGET_USE_CUSTOM_LUN_FILE_PATH := "/sys/devices/platform/msm_hsusb/gadget/lun0/file"
