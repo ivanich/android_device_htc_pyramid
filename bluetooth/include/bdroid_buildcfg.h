fd82348c9526677bb9bc8da9d9fc79773f0d8cb9
Shantur Rathore <shantur.rathore@gmail.com>
2015-10-28 22:56:45 +0000

    bluetooth: Disable BLE and deps as pyramid doesnt have it.

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -3,4 +3,7 @@
 #define BTM_DEF_LOCAL_NAME "HTC Sensation"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 #define I2SPCM_SLAVE_BRCM TRUE
+#define BLE_INCLUDED FALSE
+#define BTA_GATT_INCLUDED FALSE
+#define SMP_INCLUDED FALSE
 #endif
