2fc90dc0be787c4acb934842d72c65a0dc457a7d
Ivan Grinko <iivanich@gmail.com>
2016-02-29 22:52:27 +0200

    libshims: Drop unneeded code

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -18,9 +18,6 @@ include $(CLEAR_VARS)
 
 LOCAL_SRC_FILES := \
      gui/SensorManager.cpp \
-     ui/GraphicBuffer.cpp \
-     ui/GraphicBufferAllocator.cpp \
-     ui/GraphicBufferMapper.cpp \
      MemoryHeapPmem.cpp \
      MemoryBase.c \
      SharedBuffer.cpp \
