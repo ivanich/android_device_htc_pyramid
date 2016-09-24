3ea1a7b82e5e8ab38c568742569fe072c5cc8200
Ivan Grinko <iivanich@gmail.com>
2016-09-24 11:10:34 +0300

    bt: bluetooth: Disable CLOCK_BOOTTIME_ALARM support
    Our kernel is still using drivers/rtc/alarm.c, instead of the newer
    kernel/time/alarmtimer.c

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -7,4 +7,6 @@
 #define BTA_GATT_INCLUDED FALSE
 #define SMP_INCLUDED FALSE
 #define REMOVE_EAGER_THREADS FALSE
+/* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
+#define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
