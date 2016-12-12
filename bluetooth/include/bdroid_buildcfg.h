201c2f56862037320a74a62ac80cc365336ff933
Ivan Grinko <iivanich@gmail.com>
2016-12-12 19:10:18 +0200

    bt: Fix bt after 7.1.1 merge
    Requires hack in the hci/src/hci_hal_h4.c

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -7,6 +7,7 @@
 #define BTA_GATT_INCLUDED FALSE
 #define SMP_INCLUDED FALSE
 #define REMOVE_EAGER_THREADS FALSE
+#define LEGACY_BRCM_HCI TRUE
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
