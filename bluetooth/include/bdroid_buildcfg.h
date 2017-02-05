8592c4e0786a721781322e626b6d82d91986ba89
Ivan Grinko <iivanich@gmail.com>
2017-02-05 10:46:07 +0200

    pyramid: Bluetooth: Add Interleave scan feature
    Add Interleave Bluetooth scan feature so that both
    Bluetooth classic and LE devices can be discovered

--- bluetooth/include/bdroid_buildcfg.h
+++ bluetooth/include/bdroid_buildcfg.h
@@ -8,6 +8,7 @@
 #define SMP_INCLUDED FALSE
 #define REMOVE_EAGER_THREADS FALSE
 #define LEGACY_BRCM_HCI TRUE
+#define BTA_HOST_INTERLEAVE_SEARCH  TRUE
 /* Defined if the kernel does not have support for CLOCK_BOOTTIME_ALARM */
 #define KERNEL_MISSING_CLOCK_BOOTTIME_ALARM TRUE
 #endif
